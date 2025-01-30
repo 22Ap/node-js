a4db7c434ed021df2f2cda82a60c8e539b7d8afe
istuddious <istuddious@gmail.com>
2025-01-30 18:23:36 +0000

    Objects and Arrays Practice

--- Projects/Practice/Objects.js
+++ Projects/Practice/Objects.js
@@ -7,4 +7,24 @@ const Myself = {
     Height:5.7,
     email:'abc@gmail.com'
 };
-console.log(Myself);
+//console.log(Myself);
+//console.log(Myself.Name);
+
+// Loops in JavaScript
+// const nums=[1,2,3,'four','five','six'];
+
+//For of Loop, used for arrays
+// for(let number of nums)
+//     console.log(number);
+
+// For In Loop  used for objects;
+
+// for(let prop in Myself)
+//     console.log(prop);      // gives keys
+
+let nums2=Object.values(Myself);    //gives values of keys
+let nums3=Object.keys(Myself);      //gives keys 
+let nums4=Object.entries(Myself);   //gives both keys and values
+console.log(nums2);
+console.log(nums3);
+console.log(nums4);
