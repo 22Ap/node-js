71656a103b24582d53f6691ddc8309cf090efdca
22Ap <109693982+22Ap@users.noreply.github.com>
2025-03-06 13:49:02 +0000

    added log data in log.txt

--- Projects/Practice/httpServer.js
+++ Projects/Practice/httpServer.js
@@ -2,7 +2,18 @@ const http=require('http');
 const fs=require('fs');
 
 const myServer=http.createServer((req,res)=>{
-    res.end("Node Server Created");
+    const log=`${Date.now()}:${req.url} New Req Received\n`;
+    fs.appendFile("log.txt", log, (err)=>{
+        switch(req.url){
+            case '/' : res.end("welcome to Homepage");
+            break;
+            case '/about' : res.end("Kya kroge mere bare me jankar");
+            break;
+            case '/contact' : res.end("Musafir hu yaaro na ghar hai na thikana");
+            break;
+            default : res.end("galt jagah aa gye malik");
+        }
+    })
 })
 
 myServer.listen(8000,()=>{
