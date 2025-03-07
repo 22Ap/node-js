f864d7f132b46942f156b19b5fd0bb4b1a408225
istuddious <istuddious@gmail.com>
2025-03-07 10:35:17 +0000

    learnt url parsing

--- Projects/Practice/httpServer.js
+++ Projects/Practice/httpServer.js
@@ -1,21 +1,27 @@
 const http=require('http');
 const fs=require('fs');
+const url=require('url');
+
 const PORT = 8000;
 const hostname= "0.0.0.0"
 const myServer=http.createServer((req,res)=>{
+    if(req.url==="/favicon.ico") return res.end;
     const log=`${Date.now()}:${req.url} New Req Received\n`;
+    const myUrl=url.parse(req.url,true);    //true for parseQuery String
+    console.log(myUrl);    
     fs.appendFile("log.txt", log, (err)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
             case '/' : res.end("welcome to Homepage");
             break;
-            case '/about' : res.end("Kya kroge mere bare me jankar");
+            case '/about' : 
+            const userName=myUrl.query.myName;
+            res.end("Kya kroge mere bare me jankar, "+userName+"?");
             break;
             case '/contact' : res.end("Musafir hu yaaro na ghar hai na thikana");
             break;
             default : res.end("galt jagah aa gye malik");
         }
     })
-    res.end("Node Server Created By Great Anupam Ji 😊");
 })
 
 myServer.listen(PORT,hostname,()=>{
